134b32934cd2e0576e8d52815ed76722d50847b4
kon_k <kon_kostas@windowslive.com>
2022-04-22 02:32:59 +0300

    add Transaction Management in CommandLineRunner

--- src/main/java/gr/uom/opensource/MyRunner.java
+++ src/main/java/gr/uom/opensource/MyRunner.java
@@ -9,8 +9,10 @@ import gr.uom.opensource.student.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.Random;
 
@@ -28,25 +30,22 @@ public class MyRunner implements CommandLineRunner {
     }
 
     @Override
-    @Transactional(rollbackOn = Exception.class, value = Transactional.TxType.REQUIRES_NEW)
     public void run(String... args) throws Exception {
 
         System.out.println("START RUNNER");
 
-        for(int i=0;i<10;i++){
-            Student student = new Student();
-            student.setId(i);
-            student.setFirstName("FirstName " + i);
-            student.setLastName("LastName " + i);
-            studentRepository.save(student);
-        }
-        for(int i=0;i<10;i++){
-            Course course = new Course();
-            course.setId(i);
-            course.setName("CourseName "+i);
-            courseRepository.save(course);
-        }
+        saveStudentsToDB();
 
+        saveCoursesToDB();
+
+        saveRegistrationsToDB();
+
+        System.out.println("END RUNNER");
+
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    private void saveRegistrationsToDB() throws Exception {
         List<Course> allCourses = courseRepository.findAll();
         List<Student> allStudents = studentRepository.findAll();
 
@@ -59,12 +58,32 @@ public class MyRunner implements CommandLineRunner {
             registration.setCourse(allCourses.get(random.nextInt(allCourses.size())));
             registration.setStudent(allStudents.get(random.nextInt(allCourses.size())));
 
-//            if(i==9) throw new Exception("RANDOM EXCEPTION");
+            if(i==9) throw new Exception("RANDOM EXCEPTION");
 
             registrationRepository.save(registration);
         }
 
         System.out.println("END RUNNER");
+    }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    private void saveCoursesToDB() {
+        for(int i=0;i<10;i++){
+            Course course = new Course();
+            course.setId(i);
+            course.setName("CourseName "+i);
+            courseRepository.save(course);
         }
+    }
+
+    @Transactional(noRollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    private void saveStudentsToDB() {
+        for(int i=0;i<10;i++){
+            Student student = new Student();
+            student.setId(i);
+            student.setFirstName("FirstName " + i);
+            student.setLastName("LastName " + i);
+            studentRepository.save(student);
+        }
+    }
 }
