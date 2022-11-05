d9d477f64487361ea202f788d54f67e87e8969ab
konkos <kon_kostas@windowslive.com>
2022-11-06 00:45:02 +0200

    add request parameter to introduction

--- src/main/java/gr/uom/opensource/HelloController.java
+++ src/main/java/gr/uom/opensource/HelloController.java
@@ -1,17 +1,25 @@
 package gr.uom.opensource;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping
-    public String sayHello(){
+    public String sayHello() {
         return "Hello";
     }
 
     @GetMapping("/{user}")
-    public String sayHello(@PathVariable String user){
+    public String sayHello(@PathVariable String user) {
         return "Hello " + user;
+    }
+
+    @GetMapping("/user")
+    public String sayHelloWithParam(@RequestParam String name) {
+        return "Hello " + name;
     }
 }
